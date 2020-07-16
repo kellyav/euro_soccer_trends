2ee3e173581c5d093231dbef0e3288df3ba55b67
Alexa Kelly <kellyav@dukes.jmu.edu>
2020-07-15 21:07:28 -0400

    Update rank.sql

--- rank.sql
+++ rank.sql
@@ -1,6 +1,7 @@
 --What is the rank of matches based on number of goals scored?
 
 SELECT
+	season,
 	date,
     	(home_goal + away_goal) AS goals,
 	RANK() OVER(ORDER BY home_goal + away_goal DESC) AS goals_rank
