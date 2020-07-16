7df44fef20c37d4d44ee157f38769fb6fb2d6693
Alexa Kelly <kellyav@dukes.jmu.edu>
2020-07-15 20:32:24 -0400

    Update running_tot_2011.sql

--- running_tot_2011.sql
+++ running_tot_2011.sql
@@ -1,7 +1,7 @@
-/* calculate the running total of goals scored 
-by the FC Utrecht when they were the home team during the 2011/2012 season. 
+/* running total of goals scored by the FC Utrecht during the 2011/2012 season. 
 Do they score more goals at the end of the season as the home or away team? */
 
+-- when they were the home team:
 SELECT 
 	date,
 	home_goal,
@@ -12,6 +12,19 @@ SELECT
     AVG(home_goal) OVER(ORDER BY date 
         ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS running_avg
 FROM match
-WHERE 
-	hometeam_id = 9908 
+WHERE	hometeam_id = 9908 
     AND season = '2011/2012’;
+
+
+-- how does FC Utrecht perform when they're the away team?
+SELECT 
+    date,
+    home_goal,
+    away_goal,
+    SUM(home_goal) OVER(ORDER BY date DESC
+         ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING) AS running_total,
+    AVG(home_goal) OVER(ORDER BY date DESC
+         ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING) AS running_avg
+FROM match
+WHERE awayteam_id = 9908 
+    AND season = '2011/2012';
